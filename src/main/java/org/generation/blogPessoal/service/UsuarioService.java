a57055678d80a98f5235bd48b42b2f5c67609fec
Saturno <samuelmathias033@gmail.com>
2022-04-04 16:06:56 -0300

    @samuelsaturn [Task: att service]

--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -43,15 +43,15 @@ public class UsuarioService {
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 		if (usuario.isPresent()) {
 			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+				usuarioLogin.get()
+				.setToken(generatorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
 				usuarioLogin.get().setTipo(usuario.get().getTipo());
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
 				usuarioLogin.get().setUsuario(usuario.get().getUsuario());
-				usuarioLogin.get()
-				.setToken(generatorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
-
+				
 				return usuarioLogin;
 			}
 		}
